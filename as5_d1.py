dc66671c6e6b9bd0e90172af45668b07f035af60
sarabry17 <sarabry17@ru.is>
2018-09-04 11:09:57 +0000

    skirfa koða og tengdi við git

---
+++ as5_d1.py
@@ -0,0 +1,12 @@
+
+num_int = int(input("Input a number: ")) # Do not change this line
+while num_int > 0:
+    if True:
+        num_int = int(input("Input a number: "))
+        max_int= max[num_int]
+    else:
+       break    
+
+#max_int = max(num_int)
+# Fill in the missing code
+#print("The maximum is", max_int)    # Do not change this line
